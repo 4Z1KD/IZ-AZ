ba2adc7667602e2faac2c82434dc5aa10c079550
Gil (4Z1KD) <4z1kd@iarc.org>
2021-09-20 10:53:22 +0300

    initial commit

---
+++ IZ-AZ-RX/DisplayService.cpp
@@ -0,0 +1,63 @@
+/*
+  DisplayService.cpp - A library for printing text to lcd
+  Created by Gil 4Z1KD, December 24, 2016.
+*/
+#include "Arduino.h"
+#include "DisplayService.h"
+#include <LiquidCrystal_I2C.h>
+
+DisplayService::DisplayService()
+{
+
+}
+
+DisplayService::DisplayService(LiquidCrystal_I2C* lcdx)
+{
+  //LiquidCrystal_I2C m_lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, D7_pin);
+  m_lcd = lcdx;
+}
+
+void DisplayService::Init()
+{
+  m_lcd->begin(16, 2);
+  m_lcd->setBacklightPin(BACKLIGHT_PIN, POSITIVE);
+  m_lcd->setBacklight(HIGH);
+  m_lcd->home();
+}
+
+void DisplayService::Log(String str, int col, int row, boolean isToClear)
+{
+  if (isToClear)
+    m_lcd->clear();
+  m_lcd->setCursor(col, row);
+  m_lcd->print("                ");
+  m_lcd->setCursor(col, row);
+  m_lcd->print(str);
+}
+
+void DisplayService::Log11(String str, int col, int row, boolean isToClear, int t_delay)
+{
+  if (isToClear)
+    m_lcd->clear();
+  m_lcd->setCursor(col, row);
+  char buf[str.length() + 1];
+  str.toCharArray(buf, str.length() + 1);
+  for (int i = 0; i < str.length(); i++)
+  {
+    m_lcd->print(buf[i]);
+    delay(t_delay);
+  }
+}
+
+void DisplayService::Blink(int repeats)
+{
+  for (int i = 0; i < repeats; i++)
+  {
+    m_lcd->backlight();
+    delay(250);
+    m_lcd->noBacklight();
+    delay(250);
+  }
+  m_lcd->backlight();
+}
+
---
+++ IZ-AZ-RX/DisplayService.h
@@ -0,0 +1,33 @@
+#ifndef DisplayService_h
+#define DisplayService_h
+
+#include "Arduino.h"
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR  0x27 // <<----- Add your address here.  Find it from I2C Scanner
+#define BACKLIGHT_PIN  3
+#define En_pin  2
+#define Rw_pin  1
+#define Rs_pin  0
+#define D4_pin  4
+#define D5_pin  5
+#define D6_pin  6
+#define D7_pin  7
+
+class DisplayService
+{
+  public:
+    DisplayService();
+    DisplayService(LiquidCrystal_I2C* lcdx);
+    void Init();
+    void Log(String str, int col, int row, boolean isToClear);
+    void Log11(String str, int col, int row, boolean isToClear, int t_delay);
+    void DisplaySelectedBand(int band);
+    void DisplaySelectedAntenna(int antenna);
+    void Blink(int repeats);
+  private:
+    LiquidCrystal_I2C* m_lcd;
+};
+
+
+#endif
